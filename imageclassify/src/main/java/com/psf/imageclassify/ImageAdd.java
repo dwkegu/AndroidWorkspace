bb4eb62c85c0ffed6f0552bdcc2c7734019c5ddf
dwkegu <dwkegu@gmail.com>
2017-05-13 17:19:25 +0800

    add image file to the image base after get its features

---
+++ imageclassify/src/main/java/com/psf/imageclassify/ImageAdd.java
@@ -0,0 +1,41 @@
+package com.psf.imageclassify;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+/**
+ * users can add images to the image collection
+ * Created by psf on 2017/5/13.
+ */
+
+public class ImageAdd {
+    static final String imageInfoPath = Environment.getExternalStorageDirectory()+"/image/imageInfo.csv";
+    static final String hashCodePath = Environment.getExternalStorageDirectory()+"/image/hashcode.hc";
+    static boolean addImageInfo(ImageNet.NetResult result, String imagePath){
+        try(FileWriter mfw = new FileWriter(imageInfoPath, true); FileOutputStream fos = new FileOutputStream(hashCodePath, true)){
+            mfw.write(imagePath);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < result.topK.length; i++){
+                sb.append(result.topK[i]);
+                sb.append(',');
+            }
+            mfw.write(sb.toString());
+            fos.write(result.hashCode);
+            mfw.flush();
+            fos.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+            Log.v("imageAdd", "read imageInfo or hashcode meet error");
+            return false;
+        }
+        return true;
+    }
+}
